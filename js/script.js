86e021eb0292a36965ee7949fa7ce729d24ea901
cheerypop23 <a01082904990@gmail.com>
2024-05-22 12:22:18 +0900

    script.js

--- js/script.js
+++ js/script.js
@@ -10,7 +10,11 @@ if(winWidth < 800){
 common()
 
 function mo(){
-
+$(".side_menu_button").on("click",function(){
+    $(this).siblings(".menu").stop().slideToggle();
+    })
+ 
+  
 }
 
 
@@ -18,10 +22,21 @@ function mo(){
 
 
 function pc(){
-
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
 }
 function common(){
-
+$(".menu>li>a").on("mouseover",function(){
+    $(this).siblings(".sub").stop().slideToggle();
+    })
+    
 }
 
 
@@ -29,7 +44,19 @@ function common(){
 
 var cachedWidth = $(window).width();
 $(window).resize(function(){
-    
+    var newWidth = $(window).width();
+    if(newWidth !== cachedWidth){
+    var delay = 300;
+    var re_timer = null;
+    $(window).on('resize', function(){
+        clearTimeout(re_timer);
+        re_timer = setTimeout(function(){
+        document.location.reload();
+        }, delay);
+    });
+
+        cachedWidth = newWidth;
+    }
 });
 
 
@@ -37,3 +64,10 @@ $(window).resize(function(){
 
 
 })
+
+
+
+
+
+
+
